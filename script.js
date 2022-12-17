fd36327e9d7603d47b749e4e89af1dd08a0a9329
DPO3 <98774344+DPO3@users.noreply.github.com>
2022-12-17 09:48:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let stars = document.getElementById('stars');
+let moon = document.getElementById('moon')
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
     stars.style.left = value * 0.25 + 'px';
     stars.style.top = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+
 }
 )
