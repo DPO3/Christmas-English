6703905738045a5c7f20ab8dfdd5daf9b59876b0
DPO3 <98774344+DPO3@users.noreply.github.com>
2022-12-17 10:30:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let stars = document.getElementById('stars');
 let moon = document.getElementById('moon')
+let behind = document.getElementById('behind')
+let front = document.getElementById('front')
+let bird1 = document.getElementById('bird1')
+let bird2 = document.getElementById('bird2')
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
     stars.style.left = value * 0.25 + 'px';
     stars.style.top = value * 0.25 + 'px';
     moon.style.top = value * 1.05 + 'px';
-
+    behind.style.top = value * 0.1 + 'px';
+    front.style.top = value * 0.5 + 'px';
+    bird1.style.left = value * 0.5 + 'px';
+    bird1.style.top = value * 0.5 + 'px';
+    bird2.style.top = value * 0.5 + 'px';
+    bird2.style.right = value * 0.5 + 'px';
 }
 )
